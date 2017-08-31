1f83d8253b115d888cbcb32a9971741efd961903
StereotypicalSquirre <github@john.geek.nz>
2017-09-01 09:26:33 +1200

    Support for AU915 frequency plan
    It's not pretty, but it does work

--- src/lmic/config.h
+++ src/lmic/config.h
@@ -7,6 +7,16 @@
 
 #define CFG_eu868 1
 //#define CFG_us915 1
+//#define CFG_au915 1
+
+// CFG_au915 also requires CFG_us915 to be defined
+// It's not pretty, but it works!
+#ifdef CFG_au915
+	#ifndef CFG_us915
+		#define CFG_us915 1
+	#endif
+#endif
+
 // This is the SX1272/SX1273 radio, which is also used on the HopeRF
 // RFM92 boards.
 //#define CFG_sx1272_radio 1
